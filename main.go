618b3c3a303270e380d8ea5bf4d81c7b35411c98
yebology <139052533+yebology@users.noreply.github.com>
2025-06-16 19:15:13 +0700

    done destination model + setup main.go

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
 func main() {
-	
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "*",
+	}))
+
+	log.Fatal(app.Listen(":8080"))
 }
---
+++ model/destination.go
@@ -0,0 +1,13 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Destination struct {
+	Id				primitive.ObjectID			`json:"id" bson:"_id,omitempty"`
+	Name			string						`json:"name"`
+	City			string						`json:"city"`
+	Province		string						`json:"province"`
+	StreetName		string						`json:"streetName"`
+	Map				string						`json:"map"`
+	Images			[]string					`json:"images"`
+}
